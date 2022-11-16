6ab1f413ce93c7f4702aa6187848d5d8be82f76e
Arina-st <116604678+Arina-st@users.noreply.github.com>
2022-11-16 14:11:57 +0200

    Add files via upload
    added  graphics

--- snake.py
+++ snake.py
@@ -8,15 +8,73 @@ class SNAKE:
         self.direction = Vector2(1,0)
         self.newblock = False
 
+        self.head_up = pygame.image.load('graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('graphics/head_left.png').convert_alpha()
+
+
+        self.tail_up = pygame.image.load('graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('graphics/tail_left.png').convert_alpha()
+
+        self.body_vertical = pygame.image.load('graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('graphics/body_horizontal.png').convert_alpha()
+
+        self.body_tr = pygame.image.load('graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('graphics/body_bl.png').convert_alpha()
+    
+
     def draw_snake(self):
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
         #pentru marimea snake-ului
-        for block in self.body:
+        for index,block in enumerate(self.body):
             x_pos = int(block.x * cell_size)
             y_pos = int(block.y * cell_size)
-            #se deseneaza snake-ul ca patrate inlatuite
             block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
-            pygame.draw.rect(screen,(pygame.Color('blue')),block_rect)
+            #directia sarpelui
+            if index == 0:
+                screen.blit(self.head,block_rect)
+            elif index == len(self.body) -1:
+                screen.blit(self.tail,block_rect)
+                #snake middle blocks
+            else: 
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical,block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal,block_rect)
+                    #snake corners
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl,block_rect)  
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr,block_rect) 
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br,block_rect) 
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl,block_rect)                
+                   
+    
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1,0): self.head = self.head_left
+        elif head_relation == Vector2(-1,0): self.head = self.head_right
+        elif head_relation == Vector2(0,1): self.head = self.head_up
+        elif head_relation == Vector2(0,-1): self.head = self.head_down
 
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1,0): self.tail = self.tail_left
+        elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
+        elif tail_relation == Vector2(0,1): self.tail = self.tail_up
+        elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
     def move_snake(self):
         #se creste sarpele miscandu-l cu o pozitie in directie fara sa se taie ultimul vector
         if self.newblock == True:
@@ -42,7 +100,8 @@ class FRUCT:
     def draw_fruit(self):
         #pygame.Rect are nevoie de int dar ii dam float(valorile dintr-un vector sunt de tip float)
         fruit_rect = pygame.Rect(int(self.pos.x*cell_size),int(self.pos.y *cell_size),cell_size,cell_size)
-        pygame.draw.rect(screen,(255,0,0),fruit_rect)
+        screen.blit(apple,fruit_rect)
+        #pygame.draw.rect(screen,(255,0,0),fruit_rect)
     #se aleg x,y(coordonate dintr-un vector) la intamplare
     def randomize(self):
         self.x = random.randint(1,cell_number - 3)
@@ -57,6 +116,7 @@ class MAIN:
         self.snake.move_snake()
         self.check_collision()
     def draw_elements(self):
+        self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
     def check_collision(self):
@@ -65,6 +125,20 @@ class MAIN:
             self.fruit.randomize()
             self.snake.grow()
 
+    def draw_grass(self):
+        grass_color = (52,136,60)
+        for row in range(cell_number):
+            if row % 2 == 0:
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
+                        pygame.draw.rect(screen,grass_color,grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0:
+                             grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
+                             pygame.draw.rect(screen,grass_color,grass_rect)            
+
 pygame.init()
 cell_size = 40
 cell_number = 20
@@ -72,6 +146,8 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_number*cell_size ,cell_number*cell_size ))
 #clock este folosit pentru a stabiliza frame-rate-ul(la 60)
 clock = pygame.time.Clock()
+#graficile marului
+apple = pygame.image.load('graphics/apple.png').convert_alpha()
 #un while care merge permanent,trebuie inchis dinauntru
 SCREEN_UPDATE = pygame.USEREVENT
 #this event will trigger every 150ms
@@ -95,7 +171,7 @@ while True:
                main_game.snake.direction = (Vector2(1,0))
             if event.key == pygame.K_LEFT:
                main_game.snake.direction = Vector2(-1,0)
-    screen.fill((175,220,80))
+    screen.fill((21,119,40))
     main_game.draw_elements()
     pygame.display.update()
     clock.tick(60)
